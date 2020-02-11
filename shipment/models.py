6fdd773cd0f340628c7d2cc53c4bd109cff1cadb
Ambuja Abha Rath <ambuja@Ambujas-MacBook-Air.local>
2020-02-11 15:13:54 +0530

    initial commit

---
+++ shipment/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+from billing_details.models import Billing_details
+from order.models import Order
+from customer_details.models  import Customer_details
+from transport.models import Transport
+# Create your models here.
+class Shipment(models.Model):
+    ShipmentId = models.CharField(max_length= 120, blank= True)
+    ShipmentDate = models.DateField(auto_now= True)
+    ShipmentReference = models.CharField(max_length= 120, blank= True)
+    ShipmentItems = models.CharField(max_length = 120, blank= True)
