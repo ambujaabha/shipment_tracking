e2236ed0ca49da964ee2f3762b9b23ef2f4d8e02
Ambuja Abha Rath <ambuja@Ambujas-MacBook-Air.local>
2020-02-12 16:56:24 +0530

    initial commit

---
+++ address/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+# Create your models here.
+class Address(models.Model):
+    streetName =  models.CharField(max_length = 120, blank= True)
+    houseNumber =  models.CharField(max_length = 120, blank= True)
+    houseNumberExtended = models.CharField(max_length = 120, blank= True)
+    addressSupplement = models.CharField(max_length = 120, blank= True)
+    extraAddressInformation = models.CharField(max_length = 120, blank= True)
+    zipCode = models.IntegerField(max_length = 120, blank= True)
+    city = models.CharField(max_length = 120, blank= True)
+    countryCode = models.IntegerField(max_length = 120, blank= True)
--- shipment/models.py
+++ shipment/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.conf import settings
 from billing_details.models import Billing_details
 from order.models import Order
 from customer_details.models  import Customer_details
@@ -9,3 +10,7 @@ class Shipment(models.Model):
     ShipmentDate = models.DateField(auto_now= True)
     ShipmentReference = models.CharField(max_length= 120, blank= True)
     ShipmentItems = models.CharField(max_length = 120, blank= True)
+
+    def __str__(self):
+        return self.ShipmentId
+        
